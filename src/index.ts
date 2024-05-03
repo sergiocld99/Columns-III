e1f0e018585f5fbedcacdd9e2acb3c43872701f6
Sergio Calderón <bs.sergiocalderon@gmail.com>
2024-05-03 20:37:11 -0300

    Player 2 copies Player 1 movements

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import Block from "./block.js";
 import Board from "./board.js";
 import FallingBlock from "./fallingBlock.js";
 import NextBlock from "./nextBlock.js";
+import Player from "./player.js";
 
 function playMusic(name: string, initialSecs = 0) {
     const bgm = new Audio(`bgm/${name}.bgm`);
@@ -21,62 +22,29 @@ setTimeout(() => {
 }, 3000);
 
 
-// test images
-const leftStatsEl = document.getElementById("left_stats") as HTMLDivElement
-const leftNextEl = leftStatsEl.getElementsByClassName("next-board")[0] as HTMLCanvasElement
-const leftNextCtx = leftNextEl.getContext("2d")!
-
 let imgJewels: HTMLImageElement[] = Array(6)
 for (let i=0; i<imgJewels.length; i++) 
     imgJewels[i] = document.getElementById(`st5-${i+1}`) as HTMLImageElement
 
-// player board
-const leftBoardEl = document.getElementById("left_board") as HTMLCanvasElement
-const leftBoardCtx = leftBoardEl.getContext("2d")!
 
-function drawNextBlock() {
-    leftNextCtx.clearRect(0,0, leftNextEl.width, leftNextEl.height)
-    nextBlock.draw(imgJewels, leftNextCtx)
-}
+// SETUP FOR PLAYER 1
+let player1 = new Player(document, "left")
+player1.drawNextBlock(imgJewels)
 
-document.addEventListener("keydown", e => {
-    if (e.key === "ArrowLeft") fallingBlock.moveLeft(board)
-    else if (e.key === "ArrowRight") fallingBlock.moveRight(board)
-    else if (e.key === " ") fallingBlock.rotate()
-    else console.log(e.key)
-})
+// SETUP FOR PLAYER 2
+let player2 = new Player(document, "right", 4)
+player2.drawNextBlock(imgJewels)
 
-// SETUP
-let nextBlock = new NextBlock()
-let fallingBlock = new FallingBlock(nextBlock)
-
-nextBlock = new NextBlock()
-let board = new Board(13,6)
-drawNextBlock()
-
-// LOOPS
+// LOOPS FOR PLAYER 1
 setInterval(() => {
-    fallingBlock.row += 0.5
-
-    if (fallingBlock.row > board.getLastEmptyRow(fallingBlock.col) - 2) {
-        fallingBlock.row -= 0.5
-        
-        board.placeBlock(fallingBlock)
-        board.checkColumn(fallingBlock.col)
-        board.checkRow(fallingBlock.row)
-        board.checkRow(fallingBlock.row+1)
-        board.checkRow(fallingBlock.row+2)
-        fallingBlock = new FallingBlock(nextBlock)
-        nextBlock = new NextBlock()
-        drawNextBlock()
-    }
-}, 200)
+    player1.loop()
+    player2.loop()
+}, 500)
 
 setInterval(() => {
-    leftBoardCtx.clearRect(0,0, leftBoardEl.width, leftBoardEl.height)
+    player1.drawNextBlock(imgJewels)
+    player1.drawBoard(imgJewels)
 
-    // draw
-    board.draw(imgJewels, leftBoardCtx)
-    fallingBlock.draw(imgJewels, leftBoardCtx)
+    player2.drawNextBlock(imgJewels)
+    player2.drawBoard(imgJewels)
 }, 20)
-
---
+++ src/player.ts
@@ -0,0 +1,71 @@
+import Board from "./board.js";
+import FallingBlock from "./fallingBlock.js";
+import { COLOR_VARIANTS_COUNT } from "./jewel.js";
+import NextBlock from "./nextBlock.js";
+
+export default class Player {
+    nextBlock: NextBlock
+    fallingBlock: FallingBlock
+    board: Board
+    maxColors: number
+
+    // HTML elements
+    statsEl: HTMLDivElement
+    boardEl: HTMLCanvasElement
+    nextEl: HTMLCanvasElement
+
+    // context
+    boardCtx: CanvasRenderingContext2D
+    nextCtx: CanvasRenderingContext2D
+
+    constructor(document: Document, preffix: string, maxColors = COLOR_VARIANTS_COUNT){
+        this.maxColors = maxColors
+        this.fallingBlock = new FallingBlock(new NextBlock(maxColors))
+        this.nextBlock = new NextBlock(maxColors)
+        this.board = new Board(13,6)
+
+        // HTML references
+        this.statsEl = document.getElementById(`${preffix}_stats`) as HTMLDivElement
+        this.nextEl = this.statsEl.getElementsByClassName("next-board")[0] as HTMLCanvasElement
+        this.nextCtx = this.nextEl.getContext("2d")!
+
+        this.boardEl = document.getElementById(`${preffix}_board`) as HTMLCanvasElement
+        this.boardCtx = this.boardEl.getContext("2d")!
+
+        // KEY LISTENER
+        document.addEventListener("keydown", e => {
+            if (e.key === "ArrowLeft") this.fallingBlock.moveLeft(this.board)
+            else if (e.key === "ArrowRight") this.fallingBlock.moveRight(this.board)
+            else if (e.key === " ") this.fallingBlock.rotate()
+            else console.log(e.key)
+        })
+    }
+
+    loop(){
+        this.fallingBlock.row += 0.5
+
+        if (this.fallingBlock.row > this.board.getLastEmptyRow(this.fallingBlock.col) - 2) {
+            this.fallingBlock.row -= 0.5
+
+            this.board.placeBlock(this.fallingBlock)
+            this.board.checkColumn(this.fallingBlock.col)
+            this.board.checkRow(this.fallingBlock.row)
+            this.board.checkRow(this.fallingBlock.row+1)
+            this.board.checkRow(this.fallingBlock.row+2)
+            this.fallingBlock = new FallingBlock(this.nextBlock)
+            this.nextBlock = new NextBlock(this.maxColors)
+            //this.drawNextBlock(ctx, el, imgJewels)
+        }
+    }
+
+    drawNextBlock(imgJewels: HTMLImageElement[]) {
+        this.nextCtx.clearRect(0,0, this.nextEl.width, this.nextEl.height)
+        this.nextBlock.draw(imgJewels, this.nextCtx)
+    }
+
+    drawBoard(imgJewels: HTMLImageElement[]){
+        this.boardCtx.clearRect(0,0, this.boardEl.width, this.boardEl.height)
+        this.board.draw(imgJewels, this.boardCtx)
+        this.fallingBlock.draw(imgJewels, this.boardCtx)
+    }
+}
